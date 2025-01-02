ac74e86d4f7fa5b47350dc984224c57629f1c097
Logesh <logeshshyam21@gmail.com>
2025-01-02 20:10:42 +0530

    minor changes

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -16,10 +16,20 @@ resource "google_compute_instance" "bastion_host" {
     access_config {}
   }
 
-  metadata_startup_script = file("./bastionscript.sh")
+ metadata = {
+    startup-script = <<-EOT
+      #!/bin/bash
+      sudo apt update
+      sudo apt install curl -y
+      sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
+      sudo chmod +x kubectl
+      sudo mv kubectl /usr/local/bin/
+      kubectl version --client
+
+    EOT
 }
 
-
+}
 # resource "google_compute_instance" "jenkins_server" {
 #   name         = var.jenkinshostname
 #   machine_type = var.jenkinsmachinetype
