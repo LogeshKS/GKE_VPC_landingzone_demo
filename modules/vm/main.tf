269c54b14106079ff41376bb2728650c91dc438b
Logesh <logeshshyam21@gmail.com>
2025-01-09 15:35:43 +0530

    script update

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -17,7 +17,24 @@ resource "google_compute_instance" "bastion_host" {
   }
 
  metadata = {
-    "startup-script-url" = "gs://installation_scripts/bastionscript.sh"
+    startup-script = <<-EOT
+    #!/bin/bash
+      sudo apt-get update
+      sudo apt-get install apt-transport-https ca-certificates gnupg curl
+      curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+      echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+      sudo apt-get update && sudo apt-get install google-cloud-cli
+      sudo apt-get install google-cloud-cli-gke-gcloud-auth-plugin
+      sudo apt update
+      sudo apt install curl -y
+      sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
+      sudo chmod +x kubectl
+      sudo mv kubectl /usr/local/bin/
+      sudo snap install helm --classic
+      curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/v2.7.3/argocd-linux-amd64
+      chmod +x argocd-linux-amd64
+      sudo mv argocd-linux-amd64 /usr/local/bin/argocd
+    EOT
   }
 
 }
